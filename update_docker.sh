768ba338f8727d823df86c17aa9c19035f4d4cf6
Qi Yang <nayamama@hotmail.com>
2019-08-31 18:54:40 -0400

    add task to commit new web content to docker-compose

---
+++ update_docker.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -ex
+
+apt-get update && apt-get -y install sudo
+sudo apt-get -y install git
+
+ls -al
+
+cp -rf web-content update-git/web
+
+cd update-git
+
+git config --global user.email "nayamama@hotmail.com"
+git config --global user.name "Qi Yang"
+
+git add .
+git commit -m "new web content"
+git push
