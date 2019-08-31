522c6747833c0c50c8eb59a9b7ba5b87a4d5caaa
Qi Yang <nayamama@hotmail.com>
2019-08-31 17:21:55 -0400

    chmod +x

--- setup-test.sh
+++ setup-test.sh
@@ -5,7 +5,7 @@ set -ex
 # install chrome driver for front-end test
 apt-get update && apt-get -y install sudo
 sudo apt-get install -y unzip xvfb libxi6 libgconf-2-4
-sudo apt-get install default-jdk 
+sudo apt-get install -y default-jdk 
 
 sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
 sudo echo "deb [arch=amd64]  http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
